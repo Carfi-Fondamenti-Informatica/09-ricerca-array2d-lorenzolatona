2eca6d2873537bb18b0334119db2096abd38904a
lorenzolatona <115403824+lorenzolatona@users.noreply.github.com>
2022-12-12 16:47:50 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,20 @@
+//
+// Created by Lorenzo Latona Schettini on 12/12/2022
+//
+
+bool cerca(char lista[10][20], int &posizione, char target[]){
+    for(int i=0;i<10;i++){
+        for(int j=0;j<20;j++){
+            if(lista[i][j] != target[j]){
+                j = 20;
+            }else{
+              if(j==19){
+                  posizione = i;
+                  return true;
+              }
+            }
+        }
+    }
+    return false;
+}
 
