d161d2a78dfccab9c51efdf54f1444e8b60378d9
lorenzolatona <115403824+lorenzolatona@users.noreply.github.com>
2022-12-12 16:53:51 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -2,7 +2,7 @@
 // Created by Lorenzo Latona Schettini on 12/12/2022
 //
 
-#ifndef ESERCITAZIONE9
-#define ESERCITAZIONE9
+#ifndef ESERCITAZIONE9_lib_h
+#define ESERCITAZIONE9_lib_h
 bool cerca(char [10][20],int &,char[]);
 #endif// ESERCITAZIONE9_lib_h
